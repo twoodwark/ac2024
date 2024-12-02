9a383c54f4e51196608fd8cdcf7d5588c4f5e4fa
Toby Woodwark <toby.woodwark@virginmediao2.co.uk>
2024-12-02 15:21:50 +0000

    day 1 part 2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,18 +1,25 @@
 import scala.io.Source
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{Map, HashMap}
 
 @main def hello(): Unit =
   val left = ListBuffer[Int]()
   val right = ListBuffer[Int]()
   for (line <- Source.stdin.getLines) {
-
     val row = line.split(' ')
-    left.addOne(Integer.parseInt(row.head))
-    right.addOne(Integer.parseInt(row.last))
+    left += Integer.parseInt(row.head)
+    right += Integer.parseInt(row.last)
   }
-  println(countTotalDist(left.toList, right.toList))
+  println(similarity(left.toList, right.toList))
 
+  // part 1
 def countTotalDist(left: List[Int], right: List[Int]): Int = {
   val dists = left.sorted.zip(right.sorted).map((a, b) => (a - b).abs)
   dists.sum
+}
+
+def similarity(left: List[Int], right: List[Int]): Int = {
+  val rightSet = Map.WithDefault(new HashMap[Int, Int], _ => 0)
+  for (r <- right) rightSet(r) += 1
+  left.iterator.map(l => rightSet.get(l).getOrElse(0) * l).sum
 }
