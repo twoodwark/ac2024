f2fe74111b0e1e7b834992b290cd4286d2e4c6bb
Toby Woodwark <toby.woodwark@virginmediao2.co.uk>
2024-12-03 20:04:58 +0000

    day 3 part 2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -5,7 +5,7 @@ import scala.util.matching.Regex.Match
 object Day3 {
   def main(args: Array[String]): Unit = {
     val in = Source.stdin.mkString
-    println(countMul(in))
+    println(countMul2(in))
   }
 
   // part 1
@@ -15,5 +15,19 @@ object Day3 {
       for (m <- pat.findAllMatchIn(in))
         yield m.group(1).toInt * m.group(2).toInt
     adds.sum
+  }
+  // part 2
+  def countMul2(in: String): Int = {
+    val pat = raw"do(n't)?\(\)|mul\(([0-9]+),([0-9]+)\)".r
+    var enabled = true
+    var summed = 0
+    for (m <- pat.findAllMatchIn(in)) {
+      if (m.group(2) != null)
+        summed += (if (enabled) m.group(2).toInt * m.group(3).toInt else 0)
+      else {
+        enabled = m.group(1) == null
+      }
+    }
+    summed
   }
 }
