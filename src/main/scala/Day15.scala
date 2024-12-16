79c3e88aff7fc372b6d94379ce4de6b8f917e427
Toby Woodwark <toby.woodwark@virginmediao2.co.uk>
2024-12-16 21:29:29 +0000

    day 15 part 2

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -1,16 +1,21 @@
 import scala.io.Source
 import scala.util.chaining._
 import scala.collection.mutable
+import scala.collection.SortedSet
+import scala.collection.immutable
 /*
  */
 object Day15 extends App {
-  final val WALL = '#'
-  final val BOT = '@'
-  final val BOX = 'O'
-  final val NOTHING = '.'
+  val WALL = '#'
+  val BOT = '@'
+  val BOX = 'O'
+  val BOX_L = '['
+  val BOX_R = ']'
+  val NOTHING = '.'
   val (state, instructions) = parse
 
   part1.pipe(println)
+  part2.pipe(println)
 
   type XY = (Int, Int)
   extension [T](c: XY)(using what: Map[XY, T])
@@ -21,49 +26,91 @@ object Day15 extends App {
       grid: Map[XY, Char],
       botLoc: XY
   ) {
+    assert(grid(botLoc) == BOT)
     def print_() =
       given Map[XY, Char] = grid
       val (maxX, maxY) =
         (grid.keysIterator.map(_._1).max, grid.keysIterator.map(_._2).max)
       for
-        y <- 0 to maxX
-        x <- 0 to maxY
+        y <- 0 to maxY
+        x <- 0 to maxX
       do
         if x == 0 then println()
-        val value = (x, y).value
-        print(value)
+        print((x, y).value)
       println()
 
-    def swap(from: XY, to: XY) =
-      println(f"swqp $from $to")
-      val (oldF, oldT) = (grid(from), grid(to))
-      val g = grid.updated(from, oldT).updated(to, oldF)
-      State(g, botLoc)
+    def doubleWide =
+      State(
+        grid.flatMap { (xy, c) =>
+          val (l, r) = c match {
+            case BOT => (BOT, NOTHING)
+            case BOX => (BOX_L, BOX_R)
+            case _   => (c, c)
+          }
+          Seq(
+            (xy._1 * 2, xy._2) -> l,
+            (xy._1 * 2 + 1, xy._2) -> r
+          )
+        },
+        (botLoc._1 * 2, botLoc._2)
+      )
 
     def applyInstruction(instruction: XY): State =
       given Map[XY, Char] = grid.withDefaultValue(NOTHING)
-      val contiguousNextLocs = Iterator
-        .iterate(botLoc)(_.moveBy(instruction))
-        .takeWhile(_.value != NOTHING)
-        .toList
-      val canMoveInDir = !contiguousNextLocs.tail.map(_.value).contains(WALL)
-      if !canMoveInDir then return this
-      val newLoc = botLoc.moveBy(instruction)
-      // swap nothing with box, then repeat, etc. until nothing is at current botLoc
-      contiguousNextLocs.foldRight(State(grid, newLoc)) { (cc, s) =>
-        s.swap(cc, cc.moveBy(instruction))
-      }
+      def tileSwaps(xy: XY): Seq[(XY, XY)] =
+        // for horizontal moves we always have 1 neighbour
+        // for vertical moves we may have 1 or 2
+        val moveTile = xy -> xy.moveBy(instruction)
+        (xy.value, instruction) match {
+          case (BOX_L, (0, _)) =>
+            Seq(
+              moveTile,
+              xy.moveBy(1, 0) -> xy.moveBy(1, 0).moveBy(instruction)
+            )
+          case (BOX_R, (0, _)) =>
+            Seq(
+              moveTile,
+              xy.moveBy(-1, 0) -> xy.moveBy(-1, 0).moveBy(instruction)
+            )
+          case _ => Seq(moveTile)
+        }
+      // recursively get things that need to move
+      def nextTileSwaps(from: XY): Seq[(XY, XY)] =
+        val willMove = tileSwaps(from)
+        // terminate on walls, return them so we can find them
+        if willMove.exists(_._2.value == WALL) then willMove
+        else
+          willMove ++ willMove.flatMap {
+            case (_, r) if r.value != NOTHING => nextTileSwaps(r)
+            case _                            => Seq()
+          }
+
+      // Reverse order then dedup!
+      val swaps = immutable.ListSet.from(nextTileSwaps(botLoc).reverse)
+      if swaps.exists(_._2.value == WALL) then return this
+      assert(swaps.head._2.value == NOTHING, f"swaps ${swaps.mkString}")
+      def swap(g: Map[XY, Char], from: XY, to: XY) =
+        val (oldF, oldT) = (g(from), g(to))
+        g ++ Seq(from -> oldT, to -> oldF)
+
+      val newGrid = swaps.foldLeft(grid) { (g, sw) => swap(g, sw._1, sw._2) }
+      State(newGrid, botLoc.moveBy(instruction))
 
     def sumGPScoordinates =
-      grid.filter(_._2 == BOX).keys.map { (x, y) => 100 * y + x }.sum
+      grid.collect { case ((x, y), BOX | BOX_L) => 100 * y + x }.sum
   }
 
   def part1 =
-    state.print_()
     val finalState = instructions.foldLeft(state) { (s, c) =>
       s.applyInstruction(c)
     }
-    finalState.print_()
+    finalState.sumGPScoordinates
+
+  def part2 =
+    val wideState = state.doubleWide
+    val finalState = instructions.foldLeft(wideState) { (s, c) =>
+      s.applyInstruction(c)
+    }
     finalState.sumGPScoordinates
 
   def parse =
@@ -81,5 +128,4 @@ object Day15 extends App {
       }
 
     (State(grid, botLoc), instructions)
-
 }
